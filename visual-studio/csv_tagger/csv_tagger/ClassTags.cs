bdd74f6d50a67995e5428bf4316351b48d347528
ZiTe <honmonoh@gmail.com>
2020-07-02 13:47:57 +0800

    Add Tags class

---
+++ visual-studio/csv_tagger/csv_tagger/ClassTags.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csv_tagger
+{
+    public class Tags
+    {
+        public int serial_number { get; set; }
+        public string tags_layer0 { get; set; }
+        public string tags_layer1 { get; set; }
+        public string tags_layer2 { get; set; }
+        public string tags_layer3 { get; set; }
+        public string tags_layer4 { get; set; }
+        public string tags_layer5 { get; set; }
+        public string tags_layer6 { get; set; }
+        public string tags_layer7 { get; set; }
+        public string tags_layer8 { get; set; }
+        public string tags_layer9 { get; set; }
+    }
+}
--- visual-studio/csv_tagger/csv_tagger/Form1.cs
+++ visual-studio/csv_tagger/csv_tagger/Form1.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Globalization;
+using CsvHelper;
 
 namespace csv_tagger
 {
