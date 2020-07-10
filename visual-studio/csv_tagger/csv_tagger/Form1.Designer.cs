4d4a66d89277dc186ca889784bf803e98b337de5
ZiTe-H <honmonoh@gmail.com>
2020-07-10 21:58:01 +0800

    Update GUI

--- visual-studio/csv_tagger/csv_tagger/Form1.Designer.cs
+++ visual-studio/csv_tagger/csv_tagger/Form1.Designer.cs
@@ -28,60 +28,24 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.TreeNode treeNode8 = new System.Windows.Forms.TreeNode("Node0-0-0");
-            System.Windows.Forms.TreeNode treeNode9 = new System.Windows.Forms.TreeNode("Node0-0", new System.Windows.Forms.TreeNode[] {
-            treeNode8});
-            System.Windows.Forms.TreeNode treeNode10 = new System.Windows.Forms.TreeNode("Node0-1");
-            System.Windows.Forms.TreeNode treeNode11 = new System.Windows.Forms.TreeNode("Node0-2");
-            System.Windows.Forms.TreeNode treeNode12 = new System.Windows.Forms.TreeNode("Node0-3");
-            System.Windows.Forms.TreeNode treeNode13 = new System.Windows.Forms.TreeNode("Node0", new System.Windows.Forms.TreeNode[] {
-            treeNode9,
-            treeNode10,
-            treeNode11,
-            treeNode12});
-            System.Windows.Forms.TreeNode treeNode14 = new System.Windows.Forms.TreeNode("Node1");
-            this.treeView1 = new System.Windows.Forms.TreeView();
             this.treeViewTags = new System.Windows.Forms.TreeView();
             this.SuspendLayout();
             // 
-            // treeView1
-            // 
-            this.treeView1.Location = new System.Drawing.Point(39, 33);
-            this.treeView1.Name = "treeView1";
-            treeNode8.Name = "Node0-0-0";
-            treeNode8.Text = "Node0-0-0";
-            treeNode9.Name = "Node0-0";
-            treeNode9.Text = "Node0-0";
-            treeNode10.Name = "Node0-1";
-            treeNode10.Text = "Node0-1";
-            treeNode11.Name = "Node0-2";
-            treeNode11.Text = "Node0-2";
-            treeNode12.Name = "Node0-3";
-            treeNode12.Text = "Node0-3";
-            treeNode13.Name = "Node0";
-            treeNode13.Text = "Node0";
-            treeNode14.Name = "Node1";
-            treeNode14.Text = "Node1";
-            this.treeView1.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
-            treeNode13,
-            treeNode14});
-            this.treeView1.Size = new System.Drawing.Size(121, 97);
-            this.treeView1.TabIndex = 0;
-            // 
             // treeViewTags
             // 
-            this.treeViewTags.Location = new System.Drawing.Point(39, 202);
+            this.treeViewTags.Location = new System.Drawing.Point(13, 13);
+            this.treeViewTags.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.treeViewTags.Name = "treeViewTags";
-            this.treeViewTags.Size = new System.Drawing.Size(121, 97);
+            this.treeViewTags.Size = new System.Drawing.Size(199, 359);
             this.treeViewTags.TabIndex = 1;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 562);
             this.Controls.Add(this.treeViewTags);
-            this.Controls.Add(this.treeView1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -89,8 +53,6 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.TreeView treeView1;
         private System.Windows.Forms.TreeView treeViewTags;
     }
 }
